1326a018d77771b96d74134b057862e8b1791acd
Alvaro Garcia <derecerca@gmail.com>
2016-04-11 11:26:27 +0200

    add canary tests - in should, test format

---
+++ src/test/java/com/dodevjutsu/kata/marsrover/CanaryShould.java
@@ -0,0 +1,12 @@
+package com.dodevjutsu.kata.marsrover;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CanaryShould {
+    @Test
+    public void be_negative() {
+        assertEquals(false, false);
+    }
+}
---
+++ src/test/java/com/dodevjutsu/kata/marsrover/CanaryTest.java
@@ -0,0 +1,12 @@
+package com.dodevjutsu.kata.marsrover;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CanaryTest {
+    @Test
+    public void positive() {
+        assertEquals(true, true);
+    }
+}
