c855d27189d8677266265967ca4556dec3138675
trikitrok <trikitrok@gmail.com>
2016-04-16 16:04:14 +0200

    Refactor: using a factory function to have only one place where the Rover constructor is used

--- src/test/java/com/dodevjutsu/kata/marsrover/RoverTest.java
+++ src/test/java/com/dodevjutsu/kata/marsrover/RoverTest.java
@@ -8,46 +8,50 @@ import static org.hamcrest.core.Is.is;
 public class RoverTest {
     @Test
     public void not_moving_when_receiving_an_empty_commands_sequence() {
-        Rover rover = new Rover(1, 3, "N");
+        Rover rover = aRoverAt(1, 3, "N");
 
         rover.receive("");
 
-        assertThat(rover, is(new Rover(1, 3, "N")));
+        assertThat(rover, is(aRoverAt(1, 3, "N")));
     }
 
     @Test
     public void moving_forward_when_facing_north() {
-        Rover rover = new Rover(3, 6, "N");
+        Rover rover = aRoverAt(3, 6, "N");
 
         rover.receive("f");
 
-        assertThat(rover, is(new Rover(3, 7, "N")));
+        assertThat(rover, is(aRoverAt(3, 7, "N")));
     }
 
     @Test
     public void moving_forward_when_facing_east() {
-        Rover rover = new Rover(1, 3, "E");
+        Rover rover = aRoverAt(1, 3, "E");
 
         rover.receive("f");
 
-        assertThat(rover, is(new Rover(2, 3, "E")));
+        assertThat(rover, is(aRoverAt(2, 3, "E")));
     }
 
     @Test
     public void moving_forward_when_facing_south() {
-        Rover rover = new Rover(5, 3, "S");
+        Rover rover = aRoverAt(5, 3, "S");
 
         rover.receive("f");
 
-        assertThat(rover, is(new Rover(5, 2, "S")));
+        assertThat(rover, is(aRoverAt(5, 2, "S")));
     }
 
     @Test
     public void moving_forward_when_facing_west() {
-        Rover rover = new Rover(9,0, "W");
+        Rover rover = aRoverAt(9, 0, "W");
 
         rover.receive("f");
 
-        assertThat(rover, is(new Rover(8, 0, "W")));
+        assertThat(rover, is(aRoverAt(8, 0, "W")));
+    }
+
+    private Rover aRoverAt(int x, int y, String direction) {
+        return new Rover(x, y, direction);
     }
 }
