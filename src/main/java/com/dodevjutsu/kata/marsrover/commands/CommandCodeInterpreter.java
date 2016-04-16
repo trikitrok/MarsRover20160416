134d14c51500d8d750f31abda905e424112f797f
trikitrok <trikitrok@gmail.com>
2016-04-16 23:49:25 +0200

    Refactor: removing some magic numbers using constants

--- src/main/java/com/dodevjutsu/kata/marsrover/commands/CommandCodeInterpreter.java
+++ src/main/java/com/dodevjutsu/kata/marsrover/commands/CommandCodeInterpreter.java
@@ -8,6 +8,10 @@ import java.util.Map;
 
 public class CommandCodeInterpreter {
     private static final int DISPLACEMENT_LENGTH = 1;
+    private static final String FORWARD_MOVEMENT = "f";
+    private static final String BACKWARD_MOVEMENT = "b";
+    private static final String RIGHT_ROTATION = "r";
+    private static final String LEFT_ROTATION = "l";
     private static Map<String, Command> knownCommands = knownCommands();
 
     public static Command interpret(String commandCode) {
@@ -27,10 +31,10 @@ public class CommandCodeInterpreter {
 
     private static Map<String, Command> knownCommands() {
         Map<String, Command> knownCommands = new HashMap<>();
-        knownCommands.put("f", new Movement(DISPLACEMENT_LENGTH));
-        knownCommands.put("b", new Movement(-DISPLACEMENT_LENGTH));
-        knownCommands.put("r", new RightRotation());
-        knownCommands.put("l", new LeftRotation());
+        knownCommands.put(FORWARD_MOVEMENT, new Movement(DISPLACEMENT_LENGTH));
+        knownCommands.put(BACKWARD_MOVEMENT, new Movement(-DISPLACEMENT_LENGTH));
+        knownCommands.put(RIGHT_ROTATION, new RightRotation());
+        knownCommands.put(LEFT_ROTATION, new LeftRotation());
         return Collections.unmodifiableMap(knownCommands);
     }
 }
